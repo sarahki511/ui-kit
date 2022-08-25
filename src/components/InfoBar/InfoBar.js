01e664e6c3de08e5ef0e92e67018f8da21b40abf
Sarah Ki <svki@ucsd.edu>
2022-08-25 16:33:42 -0700

    Finish Info Bar Component

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './InfoBar.module.scss';
+import PropTypes from 'prop-types';
+
+/**
+ * Information Bars are used to deliver important message to the users. (Usually at the top of the Page)
+ * */
+const InfoBar = ({ children, className, ...props }) => {
+  const classNames = cn(styles.InfoBar, styles.className);
+  return (
+    <div className={classNames} {...props}>
+      {children}
+    </div>
+  );
+};
+
+InfoBar.propTypes = {
+  /**
+   * InfoBar content
+   * */
+  children: PropTypes.node,
+  /**
+   * Custom Class Name
+   * */
+  className: PropTypes.string,
+};
+
+export default InfoBar;
---
+++ src/components/InfoBar/InfoBar.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from './../Link/Link';
+import InfoBar from './InfoBar';
+
+export default {
+  title: 'Components/UI Elements/Info Bar',
+  component: Link,
+  docs: {},
+  parameters: {
+    componentSubtitle: 'Component',
+  },
+};
+
+export const InfoBarBasic = (args) => (
+  <InfoBar {...args}>
+    Information Bar. <Link href="#">Click Here</Link> Use with Link component
+    for more Information
+  </InfoBar>
+);
+
+InfoBarBasic.args = {};
